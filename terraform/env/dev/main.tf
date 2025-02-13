b188a7b2928579ba98084364ef52b3231cfe1dbb
DevHenriqueRamos <devhenrique.ramos@gmail.com>
2025-02-13 02:42:36 -0300

    fix variables

--- terraform/env/dev/main.tf
+++ terraform/env/dev/main.tf
@@ -1,9 +1,12 @@
+variable "image_name_dev" {
+  type = string
+}
 module "dev" {
   source         = "../../infra"
   iamRole        = "dev"
   environment    = "dev"
   container_name = "homologacao"
-  image_name = var.image_name
+  image_name = var.image_name_dev
 }
 
 output "dns_alb" {
